18eaed9713bdf06b911c42e230bd00d946ddd7a6
Christopher Wan <christopher.wan@yale.edu>
2015-09-19 12:19:29 -0700

    parse integration in backend

--- App/Pages/ProfilePage.js
+++ App/Pages/ProfilePage.js
@@ -7,6 +7,9 @@
 'use strict';
 
 var React = require('react-native');
+var Parse = require('parse/react-native');
+var ParseReact = require('parse-react/react-native');
+Parse.initialize("EvI5rKmppTeSEgJPxGQkIRV8Me5clIcwcZBwES8Z", "QOw8Kuma6j7dqo19mJOwvTbwDrp8D2g7zwS3P18k");
 
 var {
   Text,
@@ -20,9 +23,36 @@ var {
   FBSDKLoginButton,
 } = FBSDKLogin;
 
+var FBSDKCore = require('react-native-fbsdkcore');
+var {
+  FBSDKGraphRequest,
+} = FBSDKCore;
+
+
 
 var Login = React.createClass({
+
+  parseInit: function () { 
+    var user = ParseReact.Mutation.Create('User', {
+      userId: '123'
+    });
+
+    user.dispatch();
+  },
+
+  fetchFriendsRequest: function() {
+    var fetchFriends = new FBSDKGraphRequest((error, result) => {
+      if (error) {
+        alert('Error making request.');
+      } else {
+        console.log(result);
+      }
+    }, '/me/friends');
+    fetchFriends.start();
+  },
+  // Start the graph request.
   render: function() {
+    this.parseInit();
     return (
       <View>
         <FBSDKLoginButton
@@ -33,12 +63,14 @@ var Login = React.createClass({
               if (result.isCanceled) {
                 alert('Login cancelled.');
               } else {
+                fetchFriendsRequest();
                 alert('Logged in.');
               }
             }
           }}
           onLogoutFinished={() => alert('Logged out.')}
           readPermissions={["email", "user_friends"]}
+
           />
       </View>
     );
